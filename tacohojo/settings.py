5c5112842984946943b90063000b6ac871cdbf7d
taylorbakow <taylorbakow@gmail.com>
2019-04-08 15:04:35 -0600

    db

--- tacohojo/settings.py
+++ tacohojo/settings.py
@@ -86,16 +86,16 @@ WSGI_APPLICATION = 'tacohojo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'tacohojo',
-#         'USER': 'taylorbakow',
-#         'PASSWORD': 'taylorbakow',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tacohojo',
+        'USER': 'taylorbakow',
+        'PASSWORD': 'taylorbakow',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
