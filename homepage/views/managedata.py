37876376f112cffb9e1296b884a399c295845ac2
HoyongJung <jhoyong57@gmail.com>
2019-04-10 14:46:56 -0600

    hy manage page

--- homepage/views/managedata.py
+++ homepage/views/managedata.py
@@ -8,16 +8,32 @@ from django.shortcuts import redirect
 from django.http import HttpResponseRedirect
 from account import models as amod
 from django.contrib.auth import models as pmod
+from django.db.models import Q 
 
 @view_function
 def process_request(request):
-    prescribers = hmod.Prescriber.objects.all()
+    formP = PrescriberForm()
+    pList = []
+    objectType = ''
+    if request.method == 'POST':
+        print('test')
+        if len(request.POST['prescribername']) > 0:
+            print('prescriber')
+            pname = request.POST['prescribername']
+            pList = hmod.Prescriber.objects.filter( Q(Fname__icontains=pname) | Q(Lname__icontains=pname)).distinct()[0:10]
+            objectType = 'Prescriber'
+    else:
+        pass
 
-    context={
-        'prescribers': prescribers,
+    context = {
+        'formP': formP,
+        'pList': pList,
+        'objectType': objectType,
     }
-
     return request.dmp.render('managedata.html', context)
+
+class PrescriberForm(forms.Form):
+    prescribername = forms.CharField(label='', widget=forms.TextInput(attrs={'class' : 'form-control', 'name': 'Prescriber', 'placeholder' :'Search by Prescriber'}), required=False)
 
 view_function
 def create(request):
