4dd93de545349fb193e1bb390528c96ae43c30c0
taylorbakow <taylorbakow@gmail.com>
2019-04-11 20:28:50 -0600

    changes

--- tacohojo/settings.py
+++ tacohojo/settings.py
@@ -139,16 +139,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     # SECURITY WARNING: this next line must be commented out at deployment
-    # BASE_DIR,
-    os.path.join(BASE_DIR, 'static'),
+    BASE_DIR,
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     # SECURITY WARNING: this next line must be commented out at deployment
-#     BASE_DIR,
-# )
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 # A logger for DMP
